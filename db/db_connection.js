4d58c82ad2fa4f685197900b23d0fa41e38ac166
sopyan28 <sopyan28@bergen.org>
2026-06-17 22:50:19 -0400

    fix db connection for Railway

--- db/db_connection.js
+++ db/db_connection.js
@@ -4,11 +4,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const dbConfig = {
-    host: process.env.DB_HOST || "localhost",
-    port: parseInt(process.env.DB_PORT || "3306"),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: process.env.MYSQLHOST || process.env.DB_HOST || "localhost",
+    port: parseInt(process.env.MYSQLPORT || process.env.DB_PORT || "3306"),
+    user: process.env.MYSQLUSER || process.env.DB_USER,
+    password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+    database: process.env.MYSQLDATABASE || process.env.DB_DATABASE,
     connectTimeout: parseInt(process.env.DB_CONNECT_TIMEOUT || "10000")
 }
 
